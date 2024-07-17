b3a41ef1440b1d7336363036e62c2dadbac506f1
no kambing <krisnaputrapamungkas@gmail.com>
2024-07-17 11:11:28 +0700

    retrurn

--- app/Http/Controllers/DecodeController.php
+++ app/Http/Controllers/DecodeController.php
@@ -6,13 +6,9 @@ use Illuminate\Http\Request;
 
 class DecodeController extends Controller
 {
-<<<<<<< HEAD
-    
-=======
     //
     public function index()
     {
         return view('decode');
     }
->>>>>>> fd0ba585596eb5612c6f591e950099be12bd54a6
 }
