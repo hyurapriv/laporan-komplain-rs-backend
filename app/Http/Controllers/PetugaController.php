3ca5963783238af7d9bfbae31dd15d1b89c9ec28
Henry Jonathan <henryjonathanchandra12@gmail.com>
2024-07-17 11:14:37 +0700

    tes

--- app/Http/Controllers/PetugaController.php
+++ app/Http/Controllers/PetugaController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Petugas;
 use Illuminate\Http\Request;
 
 class PetugaController extends Controller
 {
-    public
+    public function index()
+    {
+        $data = Petugas::all();
+        return view('petugas', compact('data'));
+    }
 }
