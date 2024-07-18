5d7d7b717f2cf1231f9cde9f00ebdcc4d4e02040
no kambing <krisnaputrapamungkas@gmail.com>
2024-07-18 10:16:43 +0700

    pembaruan

--- app/Models/Data.php
+++ app/Models/Data.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class Data extends Model
 {
     use HasFactory;
@@ -33,4 +33,21 @@ class Data extends Model
         'datetime_pengerjaan',
         'datetime_selesai',
     ];
+
+    public static function countStatusForForm3()
+    {
+        $statusCounts = self::where('form_id', 3)
+            ->select(
+                DB::raw('SUM(CASE WHEN status = "terkirim" THEN 1 ELSE 0 END) as terkirim'),
+                DB::raw('SUM(CASE WHEN status = "Dalam Pengerjaan / Pengecekan Petugas" THEN 1 ELSE 0 END) as dalam_pengerjaan'),
+                DB::raw('SUM(CASE WHEN status = "selesai" THEN 1 ELSE 0 END) as selesai'),
+                DB::raw('SUM(CASE WHEN is_pending = 1 THEN 1 ELSE 0 END) as pending')
+            )
+            ->first();
+
+        $result = array_map('intval', $statusCounts->toArray());
+        $result['total'] = array_sum($result);
+
+        return $result;
+    }
 }
